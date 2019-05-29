6cf0e6c88f84d00068f4df558778755f6cf38da9
Vishnu R Menon <vishthemenon@gmail.com>
2019-05-29 08:42:16 +0100

    Patch

--- main.cpp
+++ main.cpp
@@ -5,9 +5,42 @@
 
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <sys/timeb.h>
 
 using namespace cv;
 using namespace std;
+
+int CLOCK()
+{
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC,  &t);
+    return (t.tv_sec * 1000)+(t.tv_nsec*1e-6);
+}
+
+double _avgdur=0;
+int _fpsstart=0;
+double _avgfps=0;
+double _fps1sec=0;
+
+double avgdur(double newdur)
+{
+    _avgdur=0.98*_avgdur+0.02*newdur;
+    return _avgdur;
+}
+
+double avgfps()
+{
+    if(CLOCK()-_fpsstart>1000)
+    {
+        _fpsstart=CLOCK();
+        _avgfps=0.7*_avgfps+0.3*_fps1sec;
+        _fps1sec=0;
+    }
+    
+    _fps1sec++;
+    return _avgfps;
+}
 
 int startMarkerTracking(const Mat &cameraMatrix, const Mat &distanceCoeffs, const float arucoSquareDimension) {
     Mat frame;
@@ -18,7 +51,7 @@ int startMarkerTracking(const Mat &cameraMatrix, const Mat &distanceCoeffs, cons
     
     auto markerDict = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
     
-    VideoCapture vid(1);
+    VideoCapture vid(0);
     
     if (!vid.isOpened()) return -1;
     
@@ -26,10 +59,17 @@ int startMarkerTracking(const Mat &cameraMatrix, const Mat &distanceCoeffs, cons
     
     vector<Vec3d> rotationVec, translationVec;
     
+    int frameno=0;
+    ofstream outfile;
+    outfile.open("VsionData.txt", fstream::out);
+    cout << "FrameNo \t Timestamp \t RunningTime \t FPS \t MarkerID \t Dist1 \t Dist2 \t Dist3\n";
+    outfile << "FrameNo \t Timestamp \t RunningTime \t FPS \t MarkerID \t Dist1 \t Dist2 \t Dist3\n";
+    
     while (true) {
+        clock_t start=CLOCK();
         if (!vid.read(frame)) break;
 //        Mat reduced_noise, processed, dst;
-        
+    
         aruco::detectMarkers(frame, markerDict, markerCorners, markerIds);
         aruco::estimatePoseSingleMarkers(markerCorners, arucoSquareDimension, cameraMatrix, distanceCoeffs, rotationVec,
                                          translationVec);
@@ -37,14 +77,45 @@ int startMarkerTracking(const Mat &cameraMatrix, const Mat &distanceCoeffs, cons
         //        Canny(reduced_noise, processed, 400, 1200, 5);
         //        dst.create( frame.size(), frame.type() );
         //        dst = Scalar::all(0);
+        double dur = CLOCK()-start;
+        auto curr = chrono::system_clock::now();
+        time_t currT = chrono::system_clock::to_time_t(curr);
+        frameno++;
+        if(markerIds.size() == 0) {
+            cout << frameno << "\t"
+                 << ctime(&currT) << "\t"
+                 << avgdur(dur) << "\t"
+                 << avgfps() << "\t"
+                 << -1 << "\t"
+                 << "0\t0\t0\t\n";
+            outfile << frameno << "\t"
+                 << ctime(&currT) << "\t"
+                 << avgdur(dur) << "\t"
+                 << avgfps() << "\t"
+                 << -1 << "\t"
+                 << "0\t0\t0\t\n";
+        }
         for (int i = 0; i < markerIds.size(); i++) {
 //            aruco::drawAxis(frame, cameraMatrix, distanceCoeffs, rotationVec[i], translationVec[i], 0.08f);
-            cout << "Marker Id: ";
+            cout << frameno << "\t"
+                 << ctime(&currT) << "\t"
+                 << avgdur(dur) << "\t"
+                 << avgfps() << "\t"
+                 << markerIds[i];
+            outfile << frameno << "\t"
+                 << ctime(&currT) << "\t"
+                 << avgdur(dur) << "\t"
+                 << avgfps() << "\t"
+                 << markerIds[i];
+            
             for(int j = 0; j < 3; ++j) {
-                cout << translationVec[i][j] << ", ";
+                cout << "\t" << translationVec[i][j];
+                outfile << "\t" << translationVec[i][j];
             }
             cout << endl;
+            outfile << endl;
         }
+
 //        imshow("Camera Feed", frame);
 //        if (waitKey(60) >= 0) break;
     }
