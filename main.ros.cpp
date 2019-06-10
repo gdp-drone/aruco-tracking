c642f78e94ddf77b6eae75b4e8229a3647507e66
Vishnu R Menon <vishthemenon@gmail.com>
2019-06-10 12:52:54 +0100

    Add SMA to smoothen the pose raw data

--- main.ros.cpp
+++ main.ros.cpp
@@ -28,16 +28,16 @@ int main(int argc, char **argv) {
   // Publish whether cam is detecting the ARtag
   ros::Publisher vishnu_cam_detection_pub = n.advertise<std_msgs::Bool>("vishnu_cam_detection", 10);
   
-  const float markerLength = 3.70;
-  const float markerSeparation = 8.70;
+  const float markerLength = 4.9;
+  const float markerSeparation = 11.80;
   const int markersXY = 2;
   CVCalibration cvl("CalibParams.txt");
-  TrackerARB tracker(cvl, markerLength, markerSeparation, markersXY, markersXY, true);
+  TrackerARB tracker(cvl, markerLength, markerSeparation, markersXY, markersXY, false);
   
   int port = argc > 1 ? stoi(argv[1]) : DEFAULT_PORT;
   
   Mat frame;
-  Vec3d tVec, rVec, ctVec;
+  Vec3d tVec, rVec, ctVec, sctVec;
   VideoCapture vid(port);
   vid.set(CAP_PROP_FRAME_WIDTH, VID_CAPTURE_WIDTH);
   ROS_INFO("Width: %f, Height: %f", vid.get(CAP_PROP_FRAME_WIDTH), vid.get(CAP_PROP_FRAME_HEIGHT));
@@ -48,10 +48,11 @@ int main(int argc, char **argv) {
     }
     if (tracker.getPose(frame, tVec, rVec) > 0) {
       tracker.correctedPose(rVec, tVec, ctVec);
-      ROS_INFO("tVec_x: %f, tVec_y: %f, tVec_z: %f", ctVec[0], ctVec[1], ctVec[2]);
-      data_msg.linear.x   = (float) (ctVec[0] /  100);
-      data_msg.linear.y   = (float) (ctVec[1] / 100);
-      data_msg.linear.z   = (float) (ctVec[2] / 100);
+      tracker.smaPose(ctVec, sctVec);
+      ROS_INFO("X: %f, Y: %f, Z: %f", sctVec[0], sctVec[1], sctVec[2]);
+      data_msg.linear.x   = (float) (sctVec[0] /  100);
+      data_msg.linear.y   = (float) (sctVec[1] / 100);
+      data_msg.linear.z   = (float) (sctVec[2] / 100);
       data_msg.angular.x  = (float) rVec[0];
       data_msg.angular.y  = (float) rVec[1];
       data_msg.angular.z  = (float) rVec[2];
